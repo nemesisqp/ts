0d327b9e132c5afe4e4e9a471b34d88411a2b929
NDQP <nhudinhquocphuong@gmail.com>
2019-06-13 16:23:04 +0700

    export delay

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,3 +1,4 @@
 export { request, requestAsync } from './request';
 export { machineId, machineIdSync } from './machine-id';
 export { LocalFileState, JsonSerializable } from './mixin';
+export { delay } from './util';
